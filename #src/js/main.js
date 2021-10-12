e119194ca598e070529f62895123bdf598dfd55a
dmitriydenisov <dmitriyd2009@yandex.ru>
2021-10-12 20:18:25 +0400

    верстка готова

--- #src/js/main.js
+++ #src/js/main.js
@@ -1,75 +1,26 @@
-$(document).ready(function() {
+const menuBtn = document.querySelector('.menu__btn');
+const navLinks = document.querySelectorAll('.menu-list__link');
 
-
-	//slide2id - плавная прокрутка по ссылкам внутри страницы
-	$("nav a,a[href='#top'],a[rel='m_PageScroll2id'],a.PageScroll2id").mPageScroll2id({
-	    highlightSelector:"nav a"
-	});
-
-
-
-	// MixItUp - фильтрация работ в портфолио
-	$('#portfolio-projects').mixItUp();
-
-
-	// FancyBox - galery
-	$(".fancybox").fancybox({
-			// Default - with fix from scroll to top
-            helpers: {
-                overlay: {
-                    locked: false
-                }
-            }
-    });
-	// End of FancyBox - galery
-
-
-	// jQuery Validate JS
-	$("#contact-form").validate({
-		rules: {
-			name: { required: true },
-			email: { required: true, email: true },
-			// skype:  { required: true },
-			// phone:  { required: true },
-			message: { required: true }
-		},
-
-		messages: {
-			name: "Пожалуйста, введите свое имя",
-			email: {
-				required: "Пожалуйста, введите свой email",
-				email: "Email адрес должен быть в формате name@domain.com . Возможно вы ввели email с ошибкой."
-			},
-			message: "Пожалуйста, введите текст сообщения"
-		},
-
-		submitHandler: function(form) {
-		  ajaxFormSubmit();
+menuBtn.addEventListener('click', () =>{
+	menuBtn.classList.toggle('menu__btn--active');
+});
+navLinks.forEach(function(navLink){
+	navLink.addEventListener('click', () => {
+		if(menuBtn.classList.contains('menu__btn--active')){
+			menuBtn.classList.remove('menu__btn--active')
 		}
+	})}
+)
 
-	})
+const anchors = document.querySelectorAll('a[href*="#"]');
 
-	// Функция AJAX запрса на сервер
-	function ajaxFormSubmit(){
-		var string = $("#contact-form").serialize(); // Соханяем данные введенные в форму в строку. 
-
-		// Формируем ajax запрос
-		$.ajax({
-			type: "POST", // Тип запроса - POST
-			url: "php/mail.php", // Куда отправляем запрос
-			data: string, // Какие даные отправляем, в данном случае отправляем переменную string
-			
-			// Функция если все прошло успешно
-			success: function(html){
-				$("#contact-form").slideUp(800);
-				$('#answer').html(html);
-			}
+for(let anchor of anchors) {
+	anchor.addEventListener('click', function(event) {
+		event.preventDefault();
+		const blockID = anchor.getAttribute('href');
+		document.querySelector('' + blockID).scrollIntoView({
+			behavior: 'smooth',
+			block: 'start'
 		});
-
-		// Чтобы по Submit больше ничего не выполнялось - делаем возврат false чтобы прервать цепчку срабатывания остальных функций
-		return false; 
-	}
-
-
-
-}); 
+	});
+};
