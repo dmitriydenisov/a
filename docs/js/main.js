7f216eb2177bcea474d517e3136285943a56b704
dmitriydenisov <dmitriyd2009@yandex.ru>
2022-06-05 22:57:59 +0400

    добавил работу мясного клуба 'мясо&хлеб'

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,26 +1,28 @@
-const menuBtn = document.querySelector('.menu__btn');
-const navLinks = document.querySelectorAll('.menu-list__link');
+const menuBtn = document.querySelector(".menu__btn");
+const navLinks = document.querySelectorAll(".menu-list__link");
 
-menuBtn.addEventListener('click', () =>{
-	menuBtn.classList.toggle('menu__btn--active');
+menuBtn.addEventListener("click", () => {
+  menuBtn.classList.toggle("menu__btn--active");
 });
-navLinks.forEach(function(navLink){
-	navLink.addEventListener('click', () => {
-		if(menuBtn.classList.contains('menu__btn--active')){
-			menuBtn.classList.remove('menu__btn--active')
-		}
-	})}
-)
+navLinks.forEach(function (navLink) {
+  navLink.addEventListener("click", () => {
+    if (menuBtn.classList.contains("menu__btn--active")) {
+      menuBtn.classList.remove("menu__btn--active");
+    }
+  });
+});
 
 const anchors = document.querySelectorAll('a[href*="#"]');
 
-for(let anchor of anchors) {
-	anchor.addEventListener('click', function(event) {
-		event.preventDefault();
-		const blockID = anchor.getAttribute('href');
-		document.querySelector('' + blockID).scrollIntoView({
-			behavior: 'smooth',
-			block: 'start'
-		});
-	});
-};
+for (let anchor of anchors) {
+  anchor.addEventListener("click", function (event) {
+    event.preventDefault();
+    const blockID = anchor.getAttribute("href");
+    document.querySelector("" + blockID).scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+  });
+}
+
+document.querySelector(".date").innerText = new Date().getFullYear();
